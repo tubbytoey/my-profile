5a917e920ae3a3d1578d42b29203a013799e884e
Nichanan.P <tubby.toey@gmail.com>
2019-06-30 21:25:44 +0700

    all done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { ContactComponent } from './contact/contact.component';
-import { SkillComponent } from './skill/skill.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     ProfileComponent,
-    ExperienceComponent,
-    ContactComponent,
-    SkillComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import experiences from '../experience/experiences.json';
+import skills from '../skill/skills.json';
+import contact from '../contact/contact.json';
+
 
 @Component({
   selector: 'app-profile',
@@ -9,13 +13,29 @@ export class ProfileComponent implements OnInit {
   profileImg = '';
   firstnameImg = '';
   lastnameImg = '';
+  experiences: object[];
+  skills: object[];
+  contact: object[] = [];
+
+
   constructor() { }
 
   ngOnInit() {
     this.profileImg = 'assets/image/avatar.jpg';
     this.firstnameImg = 'assets/image/nichanan.png';
     this.lastnameImg = 'assets/image/poovanavirote.png';
+    this.experiences = experiences;
+    this.skills = skills;
+    this.contact = contact;
 
   }
+
+  renderLinkOrPlainText(obj: any) {
+    if (obj.link) {
+      return obj.text.replace(obj.link, `<a href="${obj.to}" target="_blank">${obj.link}</a>`);
+    }
+    return obj.text;
+  }
+
 
 }
